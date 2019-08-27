75a04e361ade8a1d3bb1f1f9a1d8364c9344ae01
unknown <ricardo.valim@gmail.com>
2019-08-27 11:32:15 +0100

    writing verifier

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,58 +1,107 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import "./ERC721Mintable.sol";
+import "./verifier.sol";
+
+// -TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is CustomERC721Token {
+    // -TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        bytes32 hash;
+        address submitterAddress;
+        bool hasToken;
+    }
+
+    // -TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+    Verifier public verifier_;
+
+    // -TODO define an array of the above struct
+    Solution[] solutions;
+
+    // -TODO define a mapping to store unique solutions submitted
+    mapping (address => Solution) addressToSolution;
+    mapping (bytes32 => Solution) uniqueSolutions;
+
+    // -TODO Create an event to emit when a solution is added
+    event SolutionAdded(bytes32 token, address submitterAddress);
+
+    constructor(string memory name, string memory symbol)
+    CustomERC721Token(name, symbol)
+    public {
+        verifier_ = Verifier(msg.sender);
+    }
+
+    // -TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+    function mintVerified(address to, uint tokenId)
+    onlyOwner
+    public
+    {
+        Solution memory solution = addressToSolution[to];
+        require(solution.hash > 0, "Solution does not exist");
+        require(solution.hasToken == false, "Solution had been used for token minting already");
+        super.mint(to, tokenId);
+        addressToSolution[to].hasToken = true;
+        setTokenURI(tokenId);
+    }
 
 
+    // -TODO Create a function to add the solutions to the array and emit the event
+    function addSolution
+    (
+        uint[2] memory a,
+        uint[2] memory a_p,
+        uint[2][2] memory b,
+        uint[2] memory b_p,
+        uint[2] memory c,
+        uint[2] memory c_p,
+        uint[2] memory h,
+        uint[2] memory k,
+        uint[2] memory input
+    )
+    onlyOwner
+    public
+    {
+        bool isValid = verifier_.verifyTx(a, a_p, b, b_p, c, c_p, h, k, input);
+        require(isValid == true, "Solution is not valid");
 
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+        bytes32 token = genToken(a, a_p, b, b_p, c, c_p, h, k, input);
+        require(uniqueSolutions[token].submitterAddress == address(0), "Solution exists already");
 
+        Solution memory solution = Solution(
+            {
+                hash: token,
+                submitterAddress: msg.sender,
+                hasToken: false
+            });
 
+        solutions.push(solution);
 
-// TODO define a solutions struct that can hold an index & an address
+        addressToSolution[msg.sender] = solution;
+        uniqueSolutions[solution.hash] = solution;
 
+        emit SolutionAdded(token, msg.sender);
+    }
 
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
-
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function genToken(
+        uint[2] memory a,
+        uint[2] memory a_p,
+        uint[2][2] memory b,
+        uint[2] memory b_p,
+        uint[2] memory c,
+        uint[2] memory c_p,
+        uint[2] memory h,
+        uint[2] memory k,
+        uint[2] memory input)
+    internal
+    pure
+    returns (bytes32)
+    {
+        bytes32 keyBytes = keccak256(abi.encodePacked(a, a_p, b, b_p, c, c_p, h, k, input));
+        return keyBytes;
+    }
+}
 
 
 
